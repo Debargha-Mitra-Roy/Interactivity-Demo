b6d9ed5a658fe2578153e760d9f89f04a16c0f5e
Debargha-Mitra-Roy <developer.debarghamitraroy@gmail.com>
2023-03-06 23:38:23 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Interactivity Demo
+
+**Interactivity Demo** is a basic android application designed with **API 24 Android 7.0 Nougat**.
+
+Made with by ❤️ & ☕ by ***Debargha Mitra Roy***.
+
+<p align="center">
+    <img src="https://www.linkpicture.com/q/ss_45.png" alt="Interactivity Demo" height="500" width="250">
+</p>
---
+++ app/src/main/java/com/example/interactivitydemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.interactivitydemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText nameEditText = findViewById(R.id.nameEditText);
+        Log.i("Info", "It worked");
+        Log.i("Values", nameEditText.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
